5b73ca754f60bed62b580fbd657894cc107c6802
Julian Leal <julianleal1997@gmail.com>
2022-06-01 20:41:23 -0500

    Fixed search functionality. Restructured code. Added functionality to search function to take multiple inputs and display multiple countries at once. Added an error message for non-countries.

--- index.js
+++ index.js
@@ -3,23 +3,14 @@ const countryList = document.getElementById("list");
 const searchBar = document.getElementById('search-box');
 const divDisplay = document.getElementById('container')
 
-// let idCounter = 1;
-// let currentId = 1;
-// const displayFlag = document.getElementById('main-flag')
-// const displayName = document.getElementById('country-name')
-// const displayCapital = document.getElementById('capital')
-// const displayLanguages = document.getElementById('languages')
-//let USACounter;
-
 document.addEventListener("DOMContentLoaded", () => {
-    //this will contain all our initilization functions -> setting up event listeners for form, buttons, etc.
+    //call all the init functions to set everything into motion
     initCountries();
     initSearchBar();
-    //displayCountry(USACounter); //autoset to display the USA
 })
 
-//new function to initialize flags (countrys)
 function initCountries() {
+    //pulls data from api and calls addCountries to set up flag grid
     fetch(baseURL)
     .then(resp => resp.json())
     .then(countries => {
@@ -28,6 +19,7 @@ function initCountries() {
 }
 
 function initSearchBar() {
+    //adds eventlistener to searchbar
     searchBar.addEventListener('submit', (e) => {
         e.preventDefault();
         let input = e.target["name"].value;
@@ -36,8 +28,8 @@ function initSearchBar() {
     })
 }
 
-//new function to put countries on screen
 function addCountries(countries) {
+    //adds grid of countries to click and adds their click event listeners
     countries.forEach((country) => {
         let card = document.createElement('card');
         let div = document.createElement('div');
@@ -45,86 +37,75 @@ function addCountries(countries) {
         flagEmoji.id = 'flag-emoji'
         div.textContent = country.name.common;
         flagEmoji.textContent = country.flag;
-        //p.id = idCounter;
-        //div.id = idCounter;
         card.appendChild(flagEmoji);
         card.appendChild(div);
         countryList.appendChild(card);
-
         card.addEventListener('click', (e)=> {
             e.preventDefault()
-            removeAllChildNodes(divDisplay)
-            //adjusting values from db
-            let dLanguages = `${Object.values(country.languages)}`
-            let newLanguages = dLanguages.replace(/,/g, ', ')
-            //creating new elements every click
-            let displaySection = document.createElement('section') 
-            let divFlag = document.createElement('div')
-            let divInfo = document.createElement('div')
-            let mainFlag = document.createElement('h1')
-            let countryName = document.createElement('h3')
-            let capitalName = document.createElement('h3')
-            let languageNames = document.createElement('h3')
-            //set ids for CSS styling
-            displaySection.id = 'display'
-            divFlag.id = 'div-flag'
-            mainFlag.id = 'main-flag'
-            countryName.id = "country-name"
-            capitalName.id = "capital"
-            languageNames.id = "languages"
-            divInfo.id = 'main-info'
-            //put inner text
-            countryName.innerText = `Country: ${country.name.common}`
-            capitalName.innerText = `Capital: ${country.capital}`
-            languageNames.innerText = `Native Language(s): ${newLanguages}`
-            mainFlag.textContent = country.flag
-            //append everrything
-            divFlag.appendChild(mainFlag)
-            divInfo.appendChild(countryName)
-            divInfo.appendChild(capitalName)
-            divInfo.appendChild(languageNames)
-            displaySection.appendChild(divFlag)
-            displaySection.appendChild(divInfo)
-            divDisplay.appendChild(displaySection)
-            console.log(divDisplay)
+            displayCountry(country);
+            //displayCountry(makeCountriesArr(country));
         })
-        //idCounter++;
     });
 }
 
-function removeAllChildNodes(parent) {
+function clearNodes(parent) {
+    //to clear anything previously searched for when searching new stuff
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-function displayCountry(id) {
-    //need a default
-    fetch(baseURL + "/" + id)
-    .then(resp => resp.json())
-    .then(country => {
-        //set all the main display values to that of the country selected
-        currentId = parseInt(id);
-    })
+function displayCountry(country) { //countries
+        //clear out previous search
+        clearNodes(divDisplay);
+        //for (let country of countries) {
+        //adjusting values from db
+        let dLanguages = `${Object.values(country.languages)}`;
+        let newLanguages = dLanguages.replace(/,/g, ', ');
+        //creating new elements every click
+        let displaySection = document.createElement('section');
+        let divFlag = document.createElement('div');
+        let divInfo = document.createElement('div');
+        let mainFlag = document.createElement('h1');
+        let countryName = document.createElement('h3');
+        let capitalName = document.createElement('h3');
+        let languageNames = document.createElement('h3');
+        //adding additional info just cuz
+        
+        //set ids for CSS styling
+        displaySection.id = 'display';
+        divFlag.id = 'div-flag';
+        mainFlag.id = 'main-flag';
+        countryName.id = "country-name";
+        capitalName.id = "capital";
+        languageNames.id = "languages";
+        divInfo.id = 'main-info';
+        //put inner text
+        countryName.innerText = `Country: ${country.name.common}`;
+        capitalName.innerText = `Capital: ${country.capital}`;
+        languageNames.innerText = `Native Language(s): ${newLanguages}`;
+        mainFlag.textContent = country.flag;
+        //append everrything
+        divFlag.appendChild(mainFlag);
+        divInfo.appendChild(countryName);
+        divInfo.appendChild(capitalName);
+        divInfo.appendChild(languageNames);
+        displaySection.appendChild(divFlag);
+        displaySection.appendChild(divInfo);
+        divDisplay.appendChild(displaySection);
+    //}
 }
 
 function search(input) {
     //makes a fetch request and for all countries looks for key (via the dropdown value), and checks versus the value of the input
-    //then will clear the currently displayed countries (call another clear function) and
-    //console.log(input);
     fetch(baseURL)
     .then(resp => resp.json())
     .then(countries => {
-        let country = countries.find((country) => country["name"]["common"].toUpperCase() === input.toUpperCase());
-        if(country === undefined) {
-            alert("That's not a country. Please check your spelling.");
-        }
-        let dLanguages = `${Object.values(country.languages)}`;
-        let newLanguages = dLanguages.replace(/,/g, ', ');
-        displayName.innerText = `Country: ${country.name.common}`;
-        displayCapital.innerText = `Capital: ${country.capital}`;
-        displayLanguages.innerText = `Native Language(s): ${newLanguages}`;
-        displayFlag.textContent =  `${country.flag}`;
-
-        
-    })}
+        //for (let i = 0; i <arguments.length; i++) {
+            let countryToAdd = countries.find((country) => country["name"]["common"].toUpperCase() === input.toUpperCase());
+            //countriesArr.push(countryToAdd);
+            displayCountry(countryToAdd);
+        if(countryToAdd === undefined) {
+            alert("Apologies, there are no countries that match your query. Perhaps try checking your spelling.");
+        }})}
+    //displayCountry(makeCountriesArr(input));
--- index2.js
+++ index2.js
@@ -1,23 +1,21 @@
 const baseURL = "http://localhost:3000/countries";
 const countryList = document.getElementById("list");
 const searchBar = document.getElementById('search-box');
-let idCounter = 1;
-let currentId = 1;
-const displayFlag = document.getElementById('main-flag')
-const displayName = document.getElementById('country-name')
-const displayCapital = document.getElementById('capital')
-const displayLanguages = document.getElementById('languages')
-//let USACounter;
+const divDisplay = document.getElementById('container')
+let countriesArr = [];
+let currentOb = {};
+let countryToAdd = {};
 
 document.addEventListener("DOMContentLoaded", () => {
-    //this will contain all our initilization functions -> setting up event listeners for form, buttons, etc.
+    //call all the init functions to set everything into motion
     initCountries();
     initSearchBar();
-    //displayCountry(USACounter); //autoset to display the USA
+    search("Angola", "asdf", "Honduras");
+    //search("United States");
 })
 
-//new function to initialize flags (countrys)
 function initCountries() {
+    //pulls data from api and calls addCountries to set up flag grid
     fetch(baseURL)
     .then(resp => resp.json())
     .then(countries => {
@@ -26,6 +24,7 @@ function initCountries() {
 }
 
 function initSearchBar() {
+    //adds eventlistener to searchbar
     searchBar.addEventListener('submit', (e) => {
         e.preventDefault();
         let input = e.target["name"].value;
@@ -34,60 +33,107 @@ function initSearchBar() {
     })
 }
 
-//new function to put countries on screen
-function addCountries(countries) {
-    countries.forEach((country) => {
-        let div = document.createElement('div');
-        let card = document.createElement('card');
-        let p = document.createElement('p');
-        div.textContent = country.name.common;
-        p.textContent = country.flag;
-        p.id = idCounter;
-        div.id = idCounter;
-        card.appendChild(p);
-        card.appendChild(div);
-        countryList.appendChild(card);
-
-        card.addEventListener('click', (e)=> {
-            e.preventDefault()
-            let dLanguages = `${Object.values(country.languages)}`
-            let newLanguages = dLanguages.replace(/,/g, ', ')
-            displayName.innerText = `Country: ${country.name.common}`
-            displayCapital.innerText = `Capital: ${country.capital}`
-            displayLanguages.innerText = `Native Language(s): ${newLanguages}`
-            displayFlag.textContent = country.flag
-        })
-        idCounter++;
-    });
+async function makeCountriesArr(...args) {
+    countriesArr.splice(0, countriesArr.length);
+    await fetch(baseURL)
+        .then(resp => resp.json())
+        .then(async (countries) => {
+            let values = Object.values(args[0]);
+            for (let i = 0; i < values.length; i++) {
+                if(typeof(args[i]) === 'object'){
+                    countryToAdd = countries.find((country) => country["name"]["common"].toUpperCase() === values[i].toUpperCase());
+                    if(countryToAdd === undefined) {
+                        alert(`Apologies, there are no countries that match your query of ${values[i]}. Perhaps try checking your spelling.`);
+                    }
+                    else {countriesArr.push(countryToAdd)};
+                }
+                else {
+                    let countryToAdd = countries.find((country) => country["name"]["common"].toUpperCase() === values[i].toUpperCase());
+                if(countryToAdd === undefined) {
+                    alert(`Apologies, there are no countries that match your query of ${values[i]}. Perhaps try checking your spelling.`);
+                }
+                else {countriesArr.push(countryToAdd)};
+    }}})
+    return countriesArr;
 }
 
-function displayCountry(id) {
-    //need a default
-    fetch(baseURL + "/" + id)
-    .then(resp => resp.json())
-    .then(country => {
-        //set all the main display values to that of the country selected
-        currentId = parseInt(id);
+function addCountries(countries) {
+    //adds grid of countries to click and adds their click event listeners
+    countries.forEach((country) => {
+        let card = document.createElement('card');
+        let div = document.createElement('div');
+        let flagEmoji = document.createElement('h1');
+        flagEmoji.id = 'flag-emoji'
+        div.textContent = country.name.common;
+        flagEmoji.textContent = country.flag;
+        card.appendChild(flagEmoji);
+        card.appendChild(div);
+        countryList.appendChild(card);
+        card.addEventListener('click', async (e)=> {
+            e.preventDefault()
+            clearNodes(divDisplay);
+            //await makeCountriesArr(countrySub);
+            //for (let i = 0; i < countriesArr.length; i++){
+            displayCountry(country)//};
+        })
     })
 }
 
-function search(input) {
-    //makes a fetch request and for all countries looks for key (via the dropdown value), and checks versus the value of the input
-    //then will clear the currently displayed countries (call another clear function) and
-    //console.log(input);
-    fetch(baseURL)
-    .then(resp => resp.json())
-    .then(countries => {
-        for(let country of countries)
-            //console.log(country.name.common);
-            if (country["name"]["common"].toUpperCase() === input.toUpperCase()){
-                //displayCountry(country["name"])
-                let dLanguages = `${Object.values(country.languages)}`
-                let newLanguages = dLanguages.replace(/,/g, ', ')
-                displayName.innerText = `Country: ${country.name.common}`
-                displayCapital.innerText = `Capital: ${country.capital}`
-                displayLanguages.innerText = `Native Language(s): ${newLanguages}`
-                displayFlag.textContent =  `${country.flag}`
-            }
-        }
-)}
+function clearNodes(parent) {
+    //to clear anything previously searched for when searching new stuff
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function displayCountry(country) {
+        //adjusting values from db
+        let dLanguages = `${Object.values(country.languages)}`;
+        let newLanguages = dLanguages.replace(/,/g, ', ');
+        //creating new elements every click
+        let displaySection = document.createElement('section');
+        let divFlag = document.createElement('div');
+        let divInfo = document.createElement('div');
+        let mainFlag = document.createElement('h1');
+        let countryName = document.createElement('h3');
+        let capitalName = document.createElement('h3');
+        let languageNames = document.createElement('h3');
+        //adding additional info just cuz
+        let regions = document.createElement("h3");
+        let area = document.createElement("h3");
+        //set ids for CSS styling
+        displaySection.id = 'display';
+        divFlag.id = 'div-flag';
+        divInfo.id = 'main-info';
+        mainFlag.id = 'main-flag';
+        countryName.id = "country-name";
+        capitalName.id = "capital";
+        languageNames.id = "languages";
+        regions.id = "regions";
+        area.id = "area";
+        //put inner text
+        countryName.innerText = `Country: ${country.name.common}`;
+        capitalName.innerText = `Capital: ${country.capital}`;
+        languageNames.innerText = `Native Language(s): ${newLanguages}`;
+        regions.innerText = `Region/Subregion: ${country.region}/${country.subregion}`;
+        area.innerText = `Size (sq km): ${country.area}`;
+        mainFlag.textContent = country.flag;
+        //append everrything
+        divFlag.appendChild(mainFlag);
+        divInfo.appendChild(countryName);
+        divInfo.appendChild(capitalName);
+        divInfo.appendChild(languageNames);
+        divInfo.appendChild(area);
+        divInfo.appendChild(regions);
+        displaySection.appendChild(divFlag);
+        displaySection.appendChild(divInfo);
+        divDisplay.appendChild(displaySection);
+}
+
+async function search(...input) {
+    clearNodes(divDisplay);
+    await makeCountriesArr(input);
+    for (let i = 0; i < countriesArr.length; i++){
+        displayCountry(countriesArr[i])}
+    countriesArr.splice(0, countriesArr.length);
+}
